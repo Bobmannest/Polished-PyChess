b2ba9e2603d09b504677a076ea4c8909cf02d4f1
Kalin K <kalin.kuzev@city.ac.uk>
2025-07-08 22:33:35 +0100

    Made methods look neater
    Thats literally it

--- tile.py
+++ tile.py
@@ -7,6 +7,7 @@ d_white = (182, 219, 192)
 green = (79, 168, 103)
 d_green = (67, 143, 88)
 
+
 class Tile:
     def __init__(self, center: Vector2, color: tuple):
         self.center = center
@@ -14,10 +15,8 @@ class Tile:
 
         self.rect = pygame.Rect(self.center.x, self.center.y, 64, 64)
 
-
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect)
-
 
     def darken(self):
         if self.color == green:
@@ -25,17 +24,14 @@ class Tile:
         elif self.color == white:
             self.color = d_white
 
-
     def lighten(self):
         if self.color == d_green:
             self.color = green
         elif self.color == d_white:
             self.color = white
 
-
     def get_center(self):
         return self.center
-
 
     def get_rect(self):
         return self.rect
