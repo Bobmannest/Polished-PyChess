05197a35d60f68eb1ac3aa7c9395b15444fbca18
Kalin K <kalin.kuzev@city.ac.uk>
2025-07-12 15:34:57 +0100

    Added function
    This function checks occupied spaces and removes them from available_moves

--- piece.py
+++ piece.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Piece:
     def __init__(self, type: str):
         self.type = type
@@ -11,15 +12,17 @@ class Piece:
     def get_type(self):
         return self.type
 
+
 class Pawn(Piece):
     def get_available_moves(self, tile_pos, board):
         available_moves = []
         row, tile = tile_pos
-        if 'wt' in self.type and board[row - 1][tile].get_piece() is None:
+        if 'wt' in self.type:
             available_moves.append([row - 1, tile])
-        elif 'bk' in self.type and board[row + 1][tile].get_piece() is None:
+        elif 'bk' in self.type:
             available_moves.append([row + 1, tile])
-        return available_moves
+        return tile_occupied_check(board, available_moves)
+
 
 class Rook(Piece):
     def get_available_moves(self, tile_pos, board):
@@ -27,37 +30,58 @@ class Rook(Piece):
         row, tile = tile_pos
 
         t = tile - 1
-        while t >= 0 and board[row][t].get_piece() is None:
+        while t >= 0:
             available_moves.append([row, t])
             t -= 1
         t = tile + 1
-        while t <= 7 and board[row][t].get_piece() is None:
+        while t <= 7:
             available_moves.append([row, t])
             t += 1
 
         r = row - 1
         t = tile
-        while r >= 0 and board[r][t].get_piece() is None:
+        while r >= 0:
             available_moves.append([r, t])
             r -= 1
         r = row + 1
-        while r <= 7 and board[r][t].get_piece() is None:
+        while r <= 7:
             available_moves.append([r, t])
             r += 1
-        return available_moves
+        return tile_occupied_check(board, available_moves)
+
 
 class Knight(Piece):
     def get_available_moves(self, tile_pos, board):
-        print('knight')
+        available_moves = []
+        row, tile = tile_pos
 
+        t = tile + 2
+        available_moves.append([row + 1, t]), available_moves.append([row - 1, t])
+        t = tile - 2
+        available_moves.append([row + 1, t]), available_moves.append([row - 1, t])
+        t = tile - 1
+        available_moves.append([row + 2, t]), available_moves.append([row - 2, t])
+        t = tile + 1
+        available_moves.append([row + 2, t]), available_moves.append([row - 2, t])
+
+        return tile_occupied_check(board, available_moves)
 class Bishop(Piece):
     def get_available_moves(self, tile_pos, board):
         print('bishop')
+
 
 class Queen(Piece):
     def get_available_moves(self, tile_pos, board):
         print('queen')
 
+
 class King(Piece):
     def get_available_moves(self, tile_pos, board):
         print('king')
+
+def tile_occupied_check(board, available_moves):
+    for row in board:
+        for tile in row:
+            if tile.get_piece() is not None and tile.get_position() in available_moves:
+                available_moves.remove(tile.get_position())
+    return available_moves
